b4a1d157407d92cae0f95c92ee8559d74effb9a6
Hafizh Kamaluddin <89468371+zanayazni@users.noreply.github.com>
2023-12-12 13:54:34 +0700

    Update Privacy.jsx

--- esTehSoloProject/src/Pages/Privacy.jsx
+++ esTehSoloProject/src/Pages/Privacy.jsx
@@ -3,6 +3,7 @@ const Privacy = () => {
   return (
     <div className="privacy-landing min-h-screen justify-center flex items-center">
       <h2 className="text-4xl">Under Develop Sorry :0</h2>
+      <h2>ngengong</h2>
     </div>
   );
 };
